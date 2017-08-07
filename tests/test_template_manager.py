3fc473bb0832589f566fa7a1480600a8675a97a5
Sam Briggs <wasso14@hotmail.com>
2017-08-08 00:48:13 +0100

    Update template tests

--- tests/test_template_manager.py
+++ tests/test_template_manager.py
@@ -1,30 +1,35 @@
 import unittest
-from template_manager import TemplateManager
+from templates.template_manager import TemplateManager
+import pickle
+import os
 
-
-TEST_ISSUES = [
-    { 'title': "My first", 'number': 1},
-    { 'title': "My second", 'number': 2},
+TEST_DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_data")
+TEST_TEMPLATE_TAG = "test_template"
+FAKE_ISSUES = [
+    {'title': "Issue 1", 'number': 1},
+    {'title': "Issue 2", 'number': 2},
+    {'title': "Issue 3", 'number': 3},
 ]
 
 
 class TestTemplateManager(unittest.TestCase):
 
     def setUp(self):
-        self.builder = TemplateManager()
+        self.builder = TemplateManager(TEST_DATA_DIR)
 
     def test_simple(self):
         report = self.builder.generate(
-            'simple_report.txt',
-            TEST_ISSUES)
+            TEST_TEMPLATE_TAG,
+            FAKE_ISSUES)
 
-        # TODO: Assert contents
+        # Assert contents
         self.assertIsNotNone(report)
-        print(report)
+        self.assertEqual(report, "123")
 
     def test_list(self):
         # Get a list of templates
         templates = self.builder.list()
 
         # Assert list
-        self.assertTrue(len(templates) > 0)
+        self.assertTrue(len(templates) == 1)
+        self.assertIn('test_template', templates)
