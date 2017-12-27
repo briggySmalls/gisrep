70296f1253313c71896ed422c6b8f4c7f0c3ff35
Sam Briggs <wasso14@hotmail.com>
2017-12-27 01:51:03 +0000

    Ensure version number is consistent in setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 """Python package definition
+
+Attributes:
+    README_FILE (str): File path of project readme
+    VERSION (str): Project version number
 """
 from os import path
 from setuptools import setup, find_packages
 
 
 # Get the long description from the README file
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst'), encoding='utf-8') as file:
+README_FILE = path.join(path.abspath(path.dirname(__file__)), 'README.rst')
+with open(README_FILE, encoding='utf-8') as file:
     long_description = file.read()
 
+VERSION = '0.2.0'
 
 setup(
     name='gisrep',
-    version='0.2.0',
+    version=VERSION,
     description="The command line Github issues reporter",
     long_description=long_description,
     url="https://github.com/briggySmalls/gisrep",
-    download_url='https://github.com/briggySmalls/gisrep/archive/0.1.0.tar.gz',
+    download_url=(
+        "https://github.com/briggySmalls/gisrep/archive/"
+        "{}.tar.gz".format(VERSION)),
     author="Sam Briggs",
     author_email="briggySmalls90@gmail.com",
     license="GPLv3",
