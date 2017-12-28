3626cfd1ff968872ed1d5a453e69e30e681abf92
Sam Briggs <wasso14@hotmail.com>
2017-12-28 09:41:05 +0000

    Add test for InternalTemplateManager

--- tests/test_template_manager.py
+++ tests/test_template_manager.py
@@ -1,13 +1,15 @@
 import os
 
-from gisrep.templates.template_manager import ExternalTemplateManager
+from gisrep.templates.template_manager import (
+    InternalTemplateManager, ExternalTemplateManager)
 
 import pytest
 
 TEST_DATA_DIR = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     "test_data")
-TEST_TEMPLATE_TAG = "test_template"
+EXTERNAL_TEMPLATE_TAG = "test_template"
+INTERNAL_TEMPLATE_TAG = "simple_report.md"
 FAKE_ISSUES = [
     {'title': "Issue 1", 'number': 1},
     {'title': "Issue 2", 'number': 2},
@@ -20,14 +22,30 @@ def external_manager():
     return ExternalTemplateManager(TEST_DATA_DIR)
 
 
-def test_simple(external_manager):
+@pytest.fixture
+def internal_manager():
+    return InternalTemplateManager()
+
+
+def test_external(external_manager):
     report = external_manager.generate(
-        TEST_TEMPLATE_TAG,
+        EXTERNAL_TEMPLATE_TAG,
         FAKE_ISSUES)
 
     # Assert contents
     assert report is not None
     assert report == "123"
+
+
+def test_internal(internal_manager):
+    report = internal_manager.generate(
+        INTERNAL_TEMPLATE_TAG,
+        FAKE_ISSUES)
+
+    # Assert contents
+    assert report is not None
+    # Check line length (note: there is a blank line at end)
+    assert len(report.split('\n')) == len(FAKE_ISSUES) + 1
 
 
 def test_list(external_manager):
