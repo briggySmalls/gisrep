80c7a9800ed06b86f791464abb601a93d335f8bb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-09 07:59:12 +0000

    Deploy briggySmalls/gisrep to github.com/briggySmalls/gisrep.git:gh-pages

--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.2.1-alpha.1',
+    VERSION: '0.2.1-alpha.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
