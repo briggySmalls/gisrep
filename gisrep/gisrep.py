fbdeeebd713bd4aa926c5ec550a8fe3a134fc352
Sam Briggs <wasso14@hotmail.com>
2018-05-31 16:02:26 +0100

    Remove internal templates feature
    Instead just default to a single internal template
    We will now assume that users will supply a template for all cases where
    the default is not sufficient

--- gisrep/gisrep.py
+++ gisrep/gisrep.py
@@ -51,7 +51,7 @@ def _get_credentials(config):
     return config.get_credentials()
 
 
-def _get_template_manager(internal, external):
+def _get_template_manager(template):
     """Gets template objects
 
     Args:
@@ -61,17 +61,16 @@ def _get_template_manager(internal, external):
     Returns:
         Tuple(TemplateManager, str): Template manager and tag of template
     """
-    if external:
-        # We have been passed a template file path
-        template_dir = os.path.dirname(
-            os.path.abspath(external))
-        template_tag = os.path.splitext(
-            os.path.basename(external))[0]
-        manager = ExternalTemplateManager(template_dir)
-    else:
-        # We have been passed a template tag
-        template_tag = internal if internal is not None else 'simple_report.md'
-        manager = InternalTemplateManager()
+    if not template:
+        # No template specified so default to internal one
+        return InternalTemplateManager(), 'simple_report.md'
+
+    # We have been passed a template file path
+    template_dir = os.path.dirname(
+        os.path.abspath(template))
+    template_tag = os.path.splitext(
+        os.path.basename(template))[0]
+    manager = ExternalTemplateManager(template_dir)
 
     return manager, template_tag
 
@@ -132,52 +131,18 @@ def init(username, password, force, local):
         force=force)
 
 
-def internal_template_callback(ctx, _, value):
-    """Record if internal template is supplied
-
-    Args:
-        ctx (TYPE): Click context
-        _ (str): Parameter name
-        value (str): Tag of the internal template
-    """
-    if value != DEFAULT_TEMPLATE:
-        ctx.obj['is_internal_template'] = True
-
-
-def external_template_callback(ctx, _, value):
-    """Validate that internal and external were not both passed
-
-    Args:
-        ctx (TYPE): Click context
-        _ (str): Parameter name
-        value (click.Path): Path of the external template
-    """
-    if ctx.obj['is_internal_template'] and value:
-        # We only allow one of internal/external to be supplied
-        click.echo("Only one of --internal/--external may be supplied")
-        ctx.exit()
-
-
 @cli.command()
 @click.argument('query')
 @click.option(
-    '--external',
+    '--template',
     type=click.Path('rb'),
-    help="Custom template for formatting the results",
-    callback=external_template_callback)
-@click.option(
-    '--internal',
-    type=str,
-    default=DEFAULT_TEMPLATE,
-    help="Internal Gisrep template for formatting the results",
-    callback=internal_template_callback,
-    is_eager=True)
+    help="Custom template for formatting the results")
 @click.option(
     '--config',
     type=click.File('rb'),
     help="Path to gisrep config file")
 @click.option('--credentials', nargs=2, type=str, help="Username and password")
-def report(query, external, internal, config, credentials):
+def report(query, template, config, credentials):
     """Publishes a report of nicely formatted Github issues specified by a
     Github issues search query (see
     help.github.com/articles/searching-issues-and-pull-requests/)
@@ -206,7 +171,7 @@ def report(query, external, internal, config, credentials):
         raise GisrepError("No matching issues found")
 
     # Create the template manger
-    builder, template_tag = _get_template_manager(internal, external)
+    builder, template_tag = _get_template_manager(template)
 
     # Generate report
     report_obj = builder.generate(
@@ -215,16 +180,6 @@ def report(query, external, internal, config, credentials):
 
     # Output report
     click.echo(report_obj)
-
-
-@cli.command()
-def templates():
-    """Lists internal templates shipped with gisrep that may be used with the
-    'report' command to format the results.
-    """
-    builder = InternalTemplateManager()
-    for template in builder.list():
-        click.echo(template)
 
 
 def main():
